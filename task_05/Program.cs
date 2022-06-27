5c87618141fa0e2f062d95914ca65ffdf0eaffd7
Ellehp <Grigory3v@gmail.com>
2022-06-27 19:15:28 +0400

    обавил задачу под *

---
+++ task_05/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace dz5
+{
+class Program
+{
+static void Main(string[] args)
+{
+    
+
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int ost ;   
+  Console.WriteLine($"число {ost = number % 10} последнее в числе {number}  ");
+}
+}
+}
