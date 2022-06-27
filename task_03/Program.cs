c705cfdf78edf532379d6b18c33ff92e64e75d0c
Ellehp <Grigory3v@gmail.com>
2022-06-27 12:17:07 +0400

    добавил решение 3 задачи

---
+++ task_03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace dz3
+{
+class Program
+{
+static void Main(string[] args)
+{
+    
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 2 == 0){
+        Console.WriteLine($"число {number} чётное ");
+    }
+    else {
+        Console.WriteLine($"число {number} нечётное ");
+    }
+    
+}
+}
+}
