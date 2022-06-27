1e7ec8f5632b91c9e75a79bfc296f4ed65c1f0e8
Ellehp <Grigory3v@gmail.com>
2022-06-27 12:16:57 +0400

    добавил решение 2 задачи

---
+++ task_02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace dz2
+{
+class Program
+{
+static void Main(string[] args)
+{
+    
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите третье число");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+
+    if ( number2 > number1 ){
+        number1 = number2;
+    }   
+
+    if (number3 > number1){
+        number1 = number3;
+    }  
+
+    Console.WriteLine($"Число {number1} является болшьшим");
+}
+}
+}
