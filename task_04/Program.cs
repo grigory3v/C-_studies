0d68d1d95479ef7ea3abb49e95965333e9c79403
Ellehp <Grigory3v@gmail.com>
2022-06-27 12:17:20 +0400

    добавил решение 4 задачи

---
+++ task_04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace dz4
+{
+class Program
+{
+static void Main(string[] args)
+{
+    
+
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    for ( int count = 1; count < number; count++ )
+    {
+        if (count % 2 == 0){
+            Console.WriteLine($"число {count} чётное ");
+        }
+    }
+
+}
+}
+}
